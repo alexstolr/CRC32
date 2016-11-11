0870fe95a8c8e46d1ae6b800b20ca71d4911943c
Alex <alexstolr@gmail.com>
2016-11-11 17:42:32 +0200

    added relevant files

---
+++ crc32.cpp
@@ -0,0 +1,34 @@
+/**
+ * @author Alex Stoliar
+ * @date 25/09/16
+ * @brief Simple Calculation of CRC32 on Array of characters
+ */
+
+#include "crc32.h"
+#include <stdio.h>
+#include <inttypes.h>
+#include <iostream>
+#include <sstream>
+//#define DEBUG
+
+crc32::crc32() {
+
+}
+
+uint32_t crc32::calcCrc(char * buf,int len) {
+
+    uint32_t crc = 0xFFFFFFFFUL; // Max value of unsigned int
+    for (int i = 0; i < len; i++) {
+        crc = crc32_table[(crc ^ buf[i]) & 0xFF] ^ (crc >> 8);
+    }
+#ifdef DEBUG
+    crc = crc ^ 0xFFFFFFFFUL;
+    printf(" %0x ", (crc >> 24) & 0xFF);
+    printf(" %0x ", (crc >> 16) & 0xFF);
+    printf(" %0x ", (crc >> 8) & 0xFF);
+    printf(" %0x ", crc & 0xFF);
+    return crc;
+#else
+    return crc ^ 0xFFFFFFFFUL;
+#endif
+}
